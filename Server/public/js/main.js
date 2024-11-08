50811ed9a5d291aca2a06f3d072458904f3f6d0f
Jack Moorhouse <moverperfect@gmail.com>
2024-11-08 22:30:41 +0000

    optimise: remove unused dependencies

--- Server/public/js/main.js
+++ Server/public/js/main.js
@@ -47,23 +47,6 @@
     }
   });
 
-  // Portfolio isotope and filter
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-item',
-    layoutMode: 'fitRows',
-  });
-  $('#portfolio-flters li').on('click', function () {
-    $('#portfolio-flters li').removeClass('active');
-    $(this).addClass('active');
-
-    portfolioIsotope.isotope({ filter: $(this).data('filter') });
-  });
-  $('.portfolio-container')
-    .imagesLoaded()
-    .progress(function () {
-      portfolioIsotope.isotope('layout');
-    });
-
   // Back to top button
   $(window).scroll(function () {
     if ($(this).scrollTop() > 200) {
